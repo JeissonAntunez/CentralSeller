aebc5bf1a8999b9fc7b63660febf1bc72df3616d
JeissonAntunez <jeissonantunez@gmail.com>
2026-05-22 23:59:30 -0500

    feat(dashboard): implement layout structure and responsive navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
+import DashboardWrapper from "./dashboardWrapper";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,7 +29,8 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-        Dashboard</body>
+        <DashboardWrapper> {children}</DashboardWrapper>
+        </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 
 import  Dashboard from "@/app/dashboard/page";
+import Navbar from "@/app/(components)/Navbar/index";
 
 
 export default function Home() {
   return (
+
    
       <Dashboard />
     
+ 
   );
 }
