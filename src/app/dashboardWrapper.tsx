699aac0448908012456dc7fa7f6fb5fe8a7a276f
JeissonAntunez <jeissonantunez@gmail.com>
2026-05-25 15:22:55 -0500

    Creation component Navbar

--- src/app/dashboardWrapper.tsx
+++ src/app/dashboardWrapper.tsx
@@ -9,7 +9,7 @@ const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
             <main
                 className={`flex flex-col w-full  h-full py-7 px-9 bg-gray-50 md:pl-24`}
                 >
-                <Navbar />
+                <Navbar/>
                 {children}
             </main>
         </div>
